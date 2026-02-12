170e5689ef88fe750771665494ea0f0927242b43
unknown <franciseleazar19@gmail.com>
2026-02-12 06:06:23 +0100

    more progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from "react";
 import HabitForm from "./components/HabitForm";
 import HabitList from "./components/HabitList";
 import "./App.css";
+import { HabitProgressChart } from "./components/HabitProgressChart";
+import { createHabit } from './components/createHabit';
 
-function App() {
+function App() {  
   //using state to capture user input
   const [habits, setHabits] = useState(() => {
     const saved = localStorage.getItem("habits");
     return saved ? JSON.parse(saved) : [];
-  }, 
-);
+  });
 
   //function to add a habit
   const addHabit = (newHabit) => {
@@ -34,6 +35,24 @@ function App() {
     localStorage.setItem("habits", JSON.stringify(habits));
   }, [habits]);
 
+  // useEffect(() => {
+  //   const rawData = createHabit({ name: "", frequency: ""});
+  //   const processData = processLogsForChart(rawData);
+  //   setHabits(processData);
+  // }, []);
+
+  //processing the raw data into the chart-friendly format
+  // const processLogsForChart = (rawData) => {
+  //   //logic to map raw logs to the format
+  //   return rawData.map((log, index) => ({
+  //     name: `Day ${index + 1}`,
+  //     completed: log.isCompleted ? 1 : 0,
+  //     missed: log.isCompleted ? 0 : 1,
+  //   }));
+  // };
+
+  
+
   return (
     <div className="app">
       <h1 className="app-title">Habit Tracker</h1>
@@ -44,6 +63,13 @@ function App() {
         onRemove={removeHabit}
         onEdit={editHabit}
       />
+      <div>
+        {habits.length > 0 ? (
+          <HabitProgressChart data={habits} />
+        ) : (
+          <p>Log some habits to see your progress!</p>
+        )}
+      </div>
     </div>
   );
 }
