93b5d64441e03a8cc582d559e0e4b3463c6b7449
unknown <franciseleazar19@gmail.com>
2026-03-02 10:21:03 +0100

    rechart styling

--- src/components/HabitProgressChart.jsx
+++ src/components/HabitProgressChart.jsx
@@ -17,55 +17,29 @@ export const HabitProgressChart = ({ data }) => {
   if (!data || data.length === 0) {
     return <div className="chart-card">Loading chart...</div>;
   }
-  
+
   return (
     <div className="chart-card">
       <h2 className="chart-title">Habit Chart</h2>
       <div className="chart-container">
-        <ResponsiveContainer width="100%" aspect={2}>
-          <BarChart data={data}
-          margin={{ top: 10, right: 20, left: 0, bottom: 5 }}
-          >
-            <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis 
-            domain={[0, 1]}
-            allowDecimals={false}
-            
-            tick={{ fill: "#ccc", fontSize: 12 }}
-            axisLine={false}
-            tickLine={false}
-            />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="completed" fill="#22c55e" />
-            <Bar dataKey="missed" fill="#ef4444" />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
-    </div>
-    
-  );
-};
-
-{
-  /* <ResponsiveContainer width="100%" height={300} style={{ border: "3px solid" }}>
-          <BarChart
-          width={800}
+        <ResponsiveContainer
+          width="100%"
           height={300}
-          
+          style={{ border: "3px solid" }}
+        >
+          <BarChart
+           
             data={data}
             style={{
               border: "4px solid orange",
             }}
             margin={{
-              top: 10,
-             right: 30,
+              top: 20,
+              right: 30,
               left: 0,
-             bottom: 0,
+              bottom: 5,
             }}
-            barCategoryGap="20%"
-            barSize='10%'
+            
           >
             <defs>
               <linearGradient
@@ -75,8 +49,8 @@ export const HabitProgressChart = ({ data }) => {
                 x2="0"
                 y2="1"
               >
-                <stop offset="5%" stopColor="#4facfe" stopOpacity={0.9} />
-                <stop offset="95%" stopColor="#00f2fe" stopOpacity={0.7} />
+                <stop offset="5%" stopColor="#4bde86" stopOpacity={0.9} />
+                <stop offset="95%" stopColor="#00fecfdd" stopOpacity={0.7} />
               </linearGradient>
 
               <linearGradient id="missedGradient" x1="0" y1="0" x2="0" y2="1">
@@ -96,20 +70,21 @@ export const HabitProgressChart = ({ data }) => {
               axisLine={false}
               tickLine={false}
               dy={4}
+              padding={{ left: 0, right: 0 }}
+              scale="point"
             />
 
             <YAxis
               domain={[0, 1]}
-              width={10}
+              width={30}
               ticks={[0, 1]}
               tick={{ fill: "#ccc", fontSize: 12 }}
               axisLine={false}
               tickLine={false}
-              
             />
             <Tooltip
               contentStyle={{
-                backgroundColor: "rgba(20, 20, 30, 0.9)",
+                backgroundColor: "rgba(191, 191, 243, 0.9)",
                 border: "none",
                 borderRadius: "12px",
                 color: "#fff",
@@ -120,11 +95,10 @@ export const HabitProgressChart = ({ data }) => {
             <Legend
               wrapperStyle={{
                 color: "#ddd",
-                paddingTop: "20px"
+                paddingTop: "20px",
               }}
               verticalAlign="bottom"
               align="center"
-              // align="center"
             />
 
             <Bar
@@ -134,7 +108,7 @@ export const HabitProgressChart = ({ data }) => {
               radius={[6, 6, 0, 0]}
               isAnimationActive
               animationDuration={1000}
-              barSize={40}
+             
             />
 
             <Bar
@@ -146,5 +120,9 @@ export const HabitProgressChart = ({ data }) => {
               animationDuration={1000}
             />
           </BarChart>
-        </ResponsiveContainer> */
-}
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
